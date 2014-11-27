a001f1c7c7181a2434fcdb1775b873d0d81e84af
Viktor Chuchurski <viktor@wpn.tv>
2014-11-27 23:02:27 +0100

    Added project and documentation

---
+++ VSProjects/ApartmentComplexServiceLibrary/ApartmentComplexServiceLibrary/ServiceImplementation/ApartmentAdministrationService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ApartmentComplexServiceLibrary.ServiceInterfaces.AdministrationService;
+using ApartmentComplexServiceLibrary.Types;
+
+using MySql.Data;
+
+namespace ApartmentComplexServiceLibrary.ServiceImplementation
+{
+	class ApartmentAdministrationService : IApartmentAdministrationService
+	{
+		public AddApartmentResponse AddApartment(AddApartmentRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public FindApartmentResponse FindApartment(FindApartmentRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdateApartmentResponse UpdateApartment(UpdateApartmentRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DeleteApartmentResponse DeleteApartment(DeleteApartmentRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public AddBookingResponse AddBooking(AddBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetBookingResponse GetBooking(GetBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdateBookingResponse UpdateBooking(UpdateBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public AddPackageArrangementsResponse AddPackageArrangements(AddPackageArrangementsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetPackageArrangementsResponse GetPackageArrangements(GetPackageArrangementsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdatePackageArrangementsResponse UpdatePackageArrangements(UpdatePackageArrangementsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DeletePackageArrangementsResponse DeletePackageArrangements(DeletePackageArrangementsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public AddDiscountResponse AddDiscount(AddDiscountRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdateDiscountResponse UpdateDiscount(UpdateDiscountRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DeleteDiscountResponse DeleteDiscount(DeleteDiscountRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdateComplexInfoResponse UpdateComplexInfo(UpdateComplexInfoRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public AddCustomerResponse AddCustomer(AddCustomerRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public FindCustomerResponse FindCustomer(FindCustomerRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public UpdateCustomerResponse UpdateCustomer(UpdateCustomerRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DeleteCustomerResponse DeleteCustomer(DeleteCustomerRequest request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ VSProjects/ApartmentComplexServiceLibrary/ApartmentComplexServiceLibrary/ServiceImplementation/ApartmentPublicService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ApartmentComplexServiceLibrary.ServiceInterfaces.PublicService;
+using ApartmentComplexServiceLibrary.Types;
+
+namespace ApartmentComplexServiceLibrary.ServiceImplementation
+{
+	class ApartmentPublicService : IApartmentPublicService
+	{
+		public SearchResponse Search(SearchRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public SearchResponse AdvancedSearch(AdvancedSearchRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public MakeBookingResponse MakeBooking(MakeBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public CancelBookingResponse CancelBooking(CancelBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetBookingResponse GetBooking(GetBookingRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetBookingsForApppartmentResponse GetBookingsForApppartment(GetBookingsForApppartmentRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetBookingsForUserResponse GetBookingsForUser(GetBookingsForUserRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetPackagesArrangementsResponse GetPackagesArrangements(GetPackagesArrangementsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetDiscountsResponse GetDiscounts(GetDiscountsRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetComplexInfoResponse GetComplexInfo(GetComplexInfoRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public GetApartmentInfoResponse GetApartmentInfo(GetApartmentInfoRequest request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
